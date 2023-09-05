68e339fd6e3ec882f72f32e19c91082625de088b
szabidev <szabi.vojtel@gmail.com>
2023-09-05 11:31:41 +0300

    basic API for face recognition app

---
+++ server.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  // bcrypt
+  //   .compare(
+  //     "apples",
+  //     "$2a$10$t79turmjheafgpXdyhMq/.e9HFFfDloknso38nx3elpmFF0h6130K"
+  //   )
+  //   .then((res) => {
+  //     console.log(res);
+  //   });
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json(database.users[0]);
+  } else {
+    res.status(400).json("error logging in");
+  }
+  res.json("signin");
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(password, salt, function (err, hash) {
+      console.log(hash);
+    });
+  });
+
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    entries: 0,
+    joined: new Date(),
+  });
+
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("user not found");
+  }
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("user not found");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("works");
+});
